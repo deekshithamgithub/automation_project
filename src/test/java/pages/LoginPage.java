8ad9f34536076a2dbda04210f1b2cb8e2c135b2f
deekshithamgithub <163296974+deekshithamgithub@users.noreply.github.com>
2026-01-04 18:25:31 +0530

    Update LoginPage.java

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -2,33 +2,59 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
 
 public class LoginPage {
-
     WebDriver driver;
-
-    By username = By.name("username");
-    By password = By.name("password");
-    By loginBtn = By.xpath("//button[@type='submit']");
-    By errorMsg = By.xpath("//p[contains(@class,'alert-content-text')]");
+    WebDriverWait wait;
+    By usernameField = By.name("username");
+    By passwordField = By.name("password");
+    By loginButton = By.xpath("//button[@type='submit']");
+    By errorMessage = By.xpath("//p[contains(@class,'alert-content-text')]");
+    By requiredMessage = By.xpath("//span[text()='Required']");
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
-    public void enterUsername(String user) {
-        driver.findElement(username).sendKeys(user);
+    public void enterUsername(String username) {
+        driver.findElement(usernameField).clear();
+        driver.findElement(usernameField).sendKeys(username);
     }
 
-    public void enterPassword(String pass) {
-        driver.findElement(password).sendKeys(pass);
+    public void enterPassword(String password) {
+        driver.findElement(passwordField).clear();
+        driver.findElement(passwordField).sendKeys(password);
     }
 
-    public void clickLogin() {
-        driver.findElement(loginBtn).click();
+    public void clickLoginButton() {
+        driver.findElement(loginButton).click();
     }
 
-    public boolean isErrorDisplayed() {
-        return driver.findElement(errorMsg).isDisplayed();
+    public boolean isErrorMessageDisplayed() {
+        return wait.until(
+                ExpectedConditions.visibilityOfElementLocated(errorMessage)
+        ).isDisplayed();
     }
+
+    public boolean isPasswordMasked() {
+        WebElement pwd = driver.findElement(passwordField);
+        return pwd.getAttribute("type").equals("password");
+    }
+
+    public boolean isLoginButtonEnabled() {
+        return driver.findElement(loginButton).isEnabled();
+    }
+   
+    public boolean isDashboardDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        return wait.until(
+                ExpectedConditions.urlContains("dashboard")
+        );
+    }
+
 }
